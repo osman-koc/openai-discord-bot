3e70d2389def7ac3aadec943de1ee09e56acedd4
Osman Koç <info@osmkoc.com>
2022-04-15 02:26:20 +0300

    api integration

---
+++ api/generate.js
@@ -0,0 +1,32 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+export async function getAnswer (question) {
+    try {
+        const completion = await openai.createCompletion("text-davinci-002", {
+            prompt: question,
+            temperature: 0.6,
+            max_tokens: 600,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+            best_of: 1,
+        });
+    
+        console.log(completion);
+    
+        if(completion.status != 200) {
+            return "Sorry, I don't understand that.";
+        }
+
+        return completion.data.choices.map((choice) => choice.text).join("</br>");
+    } catch (error) {
+        console.log(error);
+        return "Sorry, I can't answer that yet. \n" + error;
+    }
+}
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-require('dotenv').config();
+import dotenv from "dotenv";
+import { getAnswer } from "./api/generate.js";
+import Discord from "discord.js";
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
-// const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+dotenv.config();
+// const client = new Discord.Client();
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 });
 
 client.on("message", msg => {
+  console.log(msg);
+
   if (msg.author.bot) return false;
 
   if (msg.content.includes("@here") || msg.content.includes("@everyone") || msg.type == "REPLY") return false;
@@ -17,9 +21,20 @@ client.on("message", msg => {
     msg.reply("pong");
   }
 
-  if (msg.mentions.has(client.user.id)) {
-    msg.channel.send("Hello there!");
+  if (msg.mentions.has(client.user.id) || msg.content.toString().includes(process.env.ROBOT_USER_ID)) {
+
+    msg.channel.send("I'm thinking...");
+    var question = msg.content.replace(client.user.id, "").replace("<@> ", "").trim();
+
+    getAnswer(question).then(result => {
+      if (result && result.trim() !== '') {
+        msg.channel.send(result);
+      }
+    });
+  } else {
+    console.log("No mention");
   }
+
 });
 
 client.login(process.env.CLIENT_TOKEN)
